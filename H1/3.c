634ceeae3afb211bf351fe7932af1b1defe3a333
Ivan Peychinov <quattrobg@abv.bg>
2012-10-08 22:20:00 +0300

    Homework 1 - 17 tasks - Vhodno nivo

--- H1/3.c
+++ H1/3.c
@@ -1,3 +1,61 @@
+/*
+Технологично училище Електронни системи - http://http://www.elsys-bg.org/
+11б клас
+№18
+Иван Пейчинов
+Да се разработи програма, която изисква от потребителя да въведе две целочислени числа, x и y, където x < y. Да се намерят и изведат всички прости числа завършващи на 3 в интервал [x,y].
+*/
+
+#include <stdio.h>
+
+int prime(int x);
+int end_three(int x);
+
+int main(){
+system("clear");
+
+int x,y;
+
+printf("Enter x : ");
+scanf("%d", &x);
+do{
+// system("clear");
+printf("Enter y>x : ");
+scanf("%d", &y);
+}while(x>=y);
+
+int i;
+for(i=x; i<y; i++){
+if( prime(i) ){
+if( end_three(i) ){
+printf("%d \n", i);
+}
+}
+}
+
+return 0;
+}
+
+int prime(int x){
+int i;
+for (i=2; i<x; i++)
+    {
+      if (x%i == 0 && i != x)
+        return 0;
+    }
+    return 1;
+}
+
+int end_three(int x){
+if( x%10 == 3 ){
+return 1;
+}
+return 0;
+}
+
+
+*/
+
 #include<stdio.h>
 
 int enterY(int x);
--- H1/6.c
+++ H1/6.c
@@ -1,3 +1,58 @@
+/*
+Технологично училище Електронни системи - http://http://www.elsys-bg.org/
+11б клас
+№18
+Иван Пейчинов
+Да се разработи програма, която изисква от потребителя да въведе целочислено число x, където 0<x<10. Да се инициализира масив от 100 целочислени елемента. Стойностите на елементите да са случайни числа в интервала [0, 100). Елементите на масива да се подредят така, че числата завършващи на x да са в началото.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+const size = 100;
+
+void sort(int *arr, int n, int x);
+
+int main(){
+system("clear");
+srand(time(NULL));
+
+int x,arr[size];
+do{
+printf("Enter 0<x<10 : ");
+scanf("%d", &x);
+}while(x<=0 || x>10);
+
+int i;
+for(i=0; i<size; i++){
+arr[i] = rand()%100;
+}
+
+sort(arr, size, x);
+
+for(i=0; i<size; i++){
+printf("%d \n", arr[i]);
+}
+
+return 0;
+}
+
+void sort(int *arr, int n, int x){
+int i,j,t;
+for(i=1; i != n; i++){
+for(j=0; j != n-1; j++){
+if( (arr[i]%10 == x) && (arr[j]%10 != x) ){
+t = arr[i];
+arr[i]=arr[j];
+arr[j]=t;
+}
+}
+}
+}
+
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
